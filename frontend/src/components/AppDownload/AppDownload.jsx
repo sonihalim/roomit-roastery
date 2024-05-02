b24a71f823ea521c688990496e2af03023cbbd9e
sonihalim <shaniluthfil@gmail.com>
2024-05-03 04:47:54 +0700

    create sign in, sign up component

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../assets/assets'
 
 const AppDownload = () => {
   return (
-    <div className='app-download' id='app-downlaod'>
+    <div className='app-download' id='app-download'>
         <p>For Better Experience Downlaod <br/>Roomit App</p>
         <div className="app-download-platforms">
             <img src={assets.play_store} alt="" />
