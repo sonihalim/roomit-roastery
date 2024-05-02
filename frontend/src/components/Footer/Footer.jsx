8616b0bb557925fdaec50ac025a77fa1389fafda
sonihalim <shaniluthfil@gmail.com>
2024-05-02 17:15:03 +0700

    make items category, cart function, and footer

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto harum veritatis quo omnis quidem quos illum illo, repudiandae vero quas!</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+62-9094-2424-4444</li>
+                    <li>contact@roomit.id</li>
+                </ul>
+            </div>
+        </div>
+        <hr />
+        <p className='footer-copyright'>
+            Copyright 2024 © Roomit.com - All Right Reserved.
+        </p>
+    </div>
+  )
+}
+
+export default Footer
