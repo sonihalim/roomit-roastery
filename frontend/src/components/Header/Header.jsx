5ee3e541dbfaf2c856936e4f7be5f614201a20f4
sonihalim <shaniluthfil@gmail.com>
2024-05-02 15:14:45 +0700

    make router pages and make home header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                Soluta velit perspiciatis eum dolor laudantium qui beatae 
+                eveniet cumque a ipsam porro esse facilis dolore nulla est 
+                enim possimus excepturi quod, at aspernatur odio veritatis. 
+                Minima optio 
+                delectus excepturi voluptate voluptatem eius voluptas quasi 
+                dolores numquam. In iure perferendis nesciunt maiores.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
