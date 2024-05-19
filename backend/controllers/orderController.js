e42c1d444b5dbbe18d1c0142f244ff258915fe40
sonihalim <shaniluthfil@gmail.com>
2024-05-20 02:48:06 +0700

    create order update status feature

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -85,5 +85,26 @@ const userOrders = async(req,res) => {
     }
 }
 
+// listing order for admin panel
+const listOrders = async (req,res) => {
+    try {
+        const orders = await orderModel.find({});
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
 
-export {placeOrder,verifyOrder,userOrders}
+// api for updating order status 
+const updateStatus = async (req,res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status});
+        res.json({success:true,message:"Status Updated"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+export {placeOrder,verifyOrder,userOrders,listOrders,updateStatus}
